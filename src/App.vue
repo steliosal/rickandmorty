a51664b32ee2b131e60c7f254f70c3e61a056c5d
HOME-PC\Stelios <steliosdirects@gmail.com>
2024-04-14 00:24:01 +0300

    Feat: Implement dynamic loader bar to enhance user feedback

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-100 text-gray-900">
     <AppHeader />
+    <BaseLoaderBar v-if="loading" />
     <main class="p-4">
       <router-view />
     </main>
@@ -9,6 +10,13 @@
 
 <script setup>
 import AppHeader from "./components/AppHeader.vue";
+import BaseLoaderBar from "./components/BaseLoaderBar.vue";
+import { storeToRefs } from "pinia";
+import { useEpisodesStore } from "@/store/episodesStore";
+
+const episodesStore = useEpisodesStore();
+
+const { loading } = storeToRefs(episodesStore);
 </script>
 
 <style scoped></style>
---
+++ src/components/BaseLoaderBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="loading" class="w-full">
+    <div class="h-1.5 w-full bg-cyan-500 overflow-hidden">
+      <div class="bg-green-500 h-full animate-progress"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useEpisodesStore } from "@/store/episodesStore";
+
+const episodesStore = useEpisodesStore();
+const loading = computed(() => episodesStore.loading);
+</script>
+
+<style>
+@keyframes progress {
+  0%,
+  10% {
+    transform: translateX(-100%) scaleX(0);
+    background: linear-gradient(to right, #32db64 0%, #4cd964 50%, #32db64 100%);
+  }
+  20% {
+    transform: translateX(0%) scaleX(1); /* Quickly expand to full width */
+    background: linear-gradient(to right, #4cd964 0%, #32db64 50%, #4cd964 100%);
+  }
+  100% {
+    transform: translateX(100%) scaleX(0);
+    background: linear-gradient(to right, #32db64 0%, #4cd964 50%, #32db64 100%);
+  }
+}
+.animate-progress {
+  width: 100%;
+  animation: progress 11s infinite ease-in-out;
+}
+</style>
